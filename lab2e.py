519cc4730c8a3d1d9a226737e4cecc131350c8e0
Karen Bay <kbay@myseneca.ca>
2025-05-19 14:30:48 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Author: Karen Bay
+
+# Author ID: kbay@myseneca
+
+# Date Created: 2025/05/19
+
+
+# Countdown timer starting at 10
+
+timer = 10
+
+
+
+while timer != 0:
+
+    print(timer)
+
+    timer = timer - 1
+
+
+print("blast off!")
